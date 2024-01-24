2368502b25d0690927302b40a4cadd69ac1f75c7
Giovani <giovani98@proton.me>
2024-01-24 10:50:35 -0300

    feat: wip - cria formulário de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,76 @@
-
-import IMGLogin from './../../assets/images/img_login.svg'
-import { ImageContainer, LoginContainer, MainWrapper } from './style';
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import IMGLogin from "./../../assets/images/img-login.svg";
+import GoogleLogo from "./../../assets/images/google-logo.svg";
+import {
+  ImageContainer,
+  LoginButton,
+  LoginContainer,
+  LoginWithGoogle,
+  MainWrapper,
+} from "./style";
+import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 export function Login() {
-    return (
-        <MainWrapper> 
-            <ImageContainer>
-                <img src={IMGLogin} alt="" />
-            </ImageContainer>
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () => setShowPassword((show) => !show);
 
-            <LoginContainer>
-                <h1>Entre no Orange Portfólio</h1>
-                Formulário
-            </LoginContainer>
-        </MainWrapper>
-    )
+  return (
+    <MainWrapper>
+      <ImageContainer>
+        <img src={IMGLogin} alt="" />
+      </ImageContainer>
+
+      <LoginContainer>
+        <form>
+          <h1>Entre no Orange Portfólio</h1>
+
+          <LoginWithGoogle type="button">Entrar com Google</LoginWithGoogle>
+
+          <h2>Faça login com email</h2>
+
+          {/* Campo para email */}
+          <TextField
+            label="Email address"
+            variant="outlined"
+            sx={{ width: "517px" }}
+          />
+
+          {/* Campo para senha, ver https://mui.com/material-ui/react-text-field/ */}
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+              sx={{ width: "517px" }}
+            />
+          </FormControl>
+
+          {/* Botão de login */}
+          <LoginButton type="submit">ENTRAR</LoginButton>
+        </form>
+      </LoginContainer>
+    </MainWrapper>
+  );
 }
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -3,14 +3,66 @@ import styled from "styled-components";
 export const MainWrapper = styled.main`
   background: ${props => props.theme.white};
   display: flex;
-  flex-direction: row;
   width: 100vw;
   height: 100vh;
   justify-content: space-between;
+  align-items: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
 `
 
-export const ImageContainer = styled.section``
+export const ImageContainer = styled.section`
+  height: 100vh;
+  overflow: hidden;
+
+  img {
+    height: 100%;
+  }
+`
 
 export const LoginContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   padding: 13.125rem 7.6875rem 12.5rem 7.6875rem;
+
+  h1 {
+    text-align: center;
+  }
+
+  h2 {
+    margin-bottom: 1.5rem;
+  }
+
+  // Essa div referencia os inputs do Material UI
+  div {
+    margin-bottom: 0.5rem;
+  }
+`
+
+export const LoginWithGoogle = styled.button`
+  margin: 2rem 0;
+`
+
+export const LoginButton = styled.button`
+  padding: 8px;
+  border: none;
+  border-radius: 4px;
+  background: ${props => props.theme["color-secondary-100"]};
+  color: ${props => props.theme["color-neutral-60"]};
+  cursor: pointer;
+
+  font-family: 'Roboto', sans-serif;
+  font-weight: medium;
+  font-size: 15px;
+
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.20), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  transition: background-color 0.1s;
+
+  &:hover {
+    background: ${props => props.theme["color-secondary-110"]};
+  }
 `
