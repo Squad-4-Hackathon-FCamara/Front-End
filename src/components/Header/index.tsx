a199f9b8578e7cca25bc1bb7f1061d3a2aceee07
Giovani <giovani98@proton.me>
2024-01-26 08:42:34 -0300

    fix: ajusta posição do menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,22 @@
+import { MouseEvent, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   HeaderContainer,
   NavigationContainer,
   ProfileContainer,
 } from "./style";
-
-import OrangeLogo from "./../../assets/images/orange-logo.svg";
-import { Menu, Notifications } from "@mui/icons-material";
 import {
   Menu as MenuComponent,
   IconButton,
   MenuItem,
   Divider,
 } from "@mui/material";
-import { MouseEvent, useEffect, useState } from "react";
+import OrangeLogo from "./../../assets/images/orange-logo.svg";
+import { Menu, Notifications } from "@mui/icons-material";
 
 export function Header() {
+  const navigate = useNavigate();
+
   // screenWidth é como uma variável;
   // setScreenWidth é uma função
   const [screenWidth, setScreenWidth] = useState(
@@ -62,19 +64,56 @@ export function Header() {
             >
               <Menu id="menu-icon"></Menu>
             </IconButton>
+            {/* 
+              anchorReference, anchorPosition, anchorOrigin e transformOrigin
+              são usadas para posicionar o menu de forma correta
+              ver: https://mui.com/material-ui/react-popover/#anchor-playground
+            */}
             <MenuComponent
               id="navigation-menu"
-              anchorEl={anchorEl}
+              anchorReference="anchorPosition"
+              anchorPosition={{ top: 74, left: 0 }}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
               open={open}
               onClose={handleClose}
               MenuListProps={{
                 "aria-labelledby": "basic-button",
               }}
             >
-              <MenuItem onClick={handleClose}>Meus Projetos</MenuItem>
-              <MenuItem onClick={handleClose}>Descobrir</MenuItem>
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  navigate("/");
+                }}
+              >
+                <p>Meus Projetos</p>
+              </MenuItem>
+
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  navigate("/discover");
+                }}
+              >
+                <p>Descobrir</p>
+              </MenuItem>
+
               <Divider />
-              <MenuItem onClick={handleClose}>Configurações</MenuItem>
+
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                }}
+              >
+                <p>Configurações</p>
+              </MenuItem>
             </MenuComponent>
           </>
         )}
