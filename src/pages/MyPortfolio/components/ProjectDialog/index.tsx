63d8790f1b49469792475f90d2ba7ae756814eab
Giovani <giovani98@proton.me>
2024-01-28 10:53:18 -0300

    feat: Adiciona Zod e RHF ao dialog de projetos

--- src/pages/MyPortfolio/components/ProjectDialog/index.tsx
+++ src/pages/MyPortfolio/components/ProjectDialog/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { ApplicationContext } from "../../../../contexts/ApplicationContext";
 import { useScreenWidth } from "../../../../hooks/useScreenWidth";
+import * as zod from "zod";
 import {
   ActionsWrapper,
   DialogContainer,
@@ -17,6 +18,8 @@ import {
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import CollectionsImage from "./../../../../assets/images/collections.svg";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 export function ProjectDialog() {
   const screenWidth = useScreenWidth();
@@ -30,6 +33,31 @@ export function ProjectDialog() {
   const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
   const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
+  const projectValidationSchema = zod.object({
+    title: zod.string().min(1, { message: "Insira o nome do projeto" }),
+    tags: zod.array(zod.string()).min(1, "Selecione uma tag"),
+    link: zod.string().min(1, { message: "Insira o link do projeto" }),
+    description: zod.string(),
+  });
+
+  type ProjectFormData = zod.infer<typeof projectValidationSchema>;
+
+  const projectForm = useForm<ProjectFormData>({
+    resolver: zodResolver(projectValidationSchema),
+    defaultValues: {
+      title: "",
+      tags: [] as string[],
+      link: "",
+      description: "",
+    },
+  });
+
+  const { register, handleSubmit } = projectForm;
+
+  function handleSaveProject(data: ProjectFormData) {
+    console.log(data);
+  }
+
   // Apenas para testes, eventualmente essas informações virão do back end
   const tagsMockUp = [
     { id: 1, name: "Front End" },
@@ -42,84 +70,91 @@ export function ProjectDialog() {
   ];
 
   return (
-    <Dialog
-      open={applicationState.addProjectDialogIsOpen}
-      onClose={handleClose}
-      maxWidth={"xl"}
-    >
-      <DialogContainer>
-        <h5>{"Adicionar Projeto"}</h5>
-        <FormWrapper>
-          <div>
-            <p>Selecione o conteúdo que você deseja fazer upload</p>
-            <ThumbnailContainer>
-              <img src={CollectionsImage} alt="" />
-              <div>
-                <p>Compartilhe seu talento com milhares de pessoas</p>
-              </div>
-            </ThumbnailContainer>
-          </div>
+    <Dialog open={applicationState.addProjectDialogIsOpen} maxWidth={"xl"}>
+      <form>
+        <DialogContainer>
+          <h5>{"Adicionar Projeto"}</h5>
+          <FormWrapper>
+            <div>
+              <p>Selecione o conteúdo que você deseja fazer upload</p>
+              <ThumbnailContainer>
+                <img src={CollectionsImage} alt="" />
+                <div>
+                  <p>Compartilhe seu talento com milhares de pessoas</p>
+                </div>
+              </ThumbnailContainer>
+            </div>
 
-          <div id="fields-wrapper">
-            <TextField
-              label="Título"
-              sx={{ width: screenWidth < 960 ? "100%" : "413px" }}
-            />
-            <Autocomplete
-              multiple
-              freeSolo
-              limitTags={screenWidth < 960 ? 1 : 3}
-              disableCloseOnSelect
-              options={tagsMockUp}
-              getOptionLabel={(tags) =>
-                typeof tags === "string" ? tags : tags.name
-              }
-              sx={{ width: screenWidth < 960 ? "100%" : "413px" }}
-              renderInput={(params) => (
-                <TextField {...params} label="Buscar tags" placeholder="" />
-              )}
-              renderOption={(props, option, { selected }) => (
-                <li {...props}>
-                  <Checkbox
-                    icon={icon}
-                    checkedIcon={checkedIcon}
-                    style={{ marginRight: 8 }}
-                    checked={selected}
-                  />
-                  {option.name}
-                </li>
-              )}
-            />
-            <TextField
-              label="Link"
-              sx={{ width: screenWidth < 960 ? "100%" : "413px" }}
-            ></TextField>
-            <TextField
-              label="Descrição"
-              multiline
-              rows={4}
-              maxRows={4}
-              sx={{ width: screenWidth < 960 ? "100%" : "413px" }}
-            />
-          </div>
-        </FormWrapper>
+            <div id="fields-wrapper">
+              <TextField
+                {...register("title")}
+                label="Título"
+                sx={{ width: screenWidth < 960 ? "100%" : "413px" }}
+              />
+              <Autocomplete
+                {...register("tags")}
+                multiple
+                freeSolo
+                limitTags={screenWidth < 960 ? 1 : 3}
+                disableCloseOnSelect
+                options={tagsMockUp}
+                getOptionLabel={(tags) =>
+                  typeof tags === "string" ? tags : tags.name
+                }
+                sx={{ width: screenWidth < 960 ? "100%" : "413px" }}
+                renderInput={(params) => (
+                  <TextField {...params} label="Buscar tags" placeholder="" />
+                )}
+                renderOption={(props, option, { selected }) => (
+                  <li {...props}>
+                    <Checkbox
+                      icon={icon}
+                      checkedIcon={checkedIcon}
+                      style={{ marginRight: 8 }}
+                      checked={selected}
+                    />
+                    {option.name}
+                  </li>
+                )}
+              />
+              <TextField
+                {...register("link")}
+                label="Link"
+                sx={{ width: screenWidth < 960 ? "100%" : "413px" }}
+              ></TextField>
+              <TextField
+                {...register("description")}
+                label="Descrição"
+                multiline
+                rows={4}
+                sx={{ width: screenWidth < 960 ? "100%" : "413px" }}
+              />
+            </div>
+          </FormWrapper>
 
-        <ActionsWrapper>
-          <p>Visualizar publicação</p>
-          <div>
-            <Button id="action-button" variant="contained">
-              Salvar
-            </Button>
-            <Button
-              id="action-button"
-              variant="contained"
-              onClick={handleClose}
-            >
-              Cancelar
-            </Button>
-          </div>
-        </ActionsWrapper>
-      </DialogContainer>
+          <ActionsWrapper>
+            <p>Visualizar publicação</p>
+            <div>
+              <Button
+                id="action-button"
+                variant="contained"
+                type="submit"
+                onClick={handleSubmit(handleSaveProject)}
+              >
+                Salvar
+              </Button>
+              <Button
+                id="action-button"
+                variant="contained"
+                onClick={handleClose}
+                type="button"
+              >
+                Cancelar
+              </Button>
+            </div>
+          </ActionsWrapper>
+        </DialogContainer>
+      </form>
     </Dialog>
   );
 }
