ade17641b053d7d39c43ca71ddade2a48e92921b
Giovani <giovani98@proton.me>
2024-02-01 10:24:24 -0300

    feat: Verifica se usuário está logado, e redireciona quando necessário

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,26 +1,51 @@
-import { useEffect } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import { Login } from "./pages/Login"
-import { Register } from "./pages/Register"
-import { MyPortfolio } from "./pages/MyPortfolio"
-import { Discover } from "./pages/Discover"
-import { DefaultLayout } from './layouts/DefaultLayout';
+import { useEffect } from 'react'
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { MyPortfolio } from './pages/MyPortfolio'
+import { Discover } from './pages/Discover'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
-	const { pathname } = useLocation()
+  const { pathname } = useLocation()
 
-	useEffect(() => {
-		window.scrollTo(0, 0)
-	}, [pathname])
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
 
-	return (
-		<Routes>
-			<Route path="/login" element={<Login />}></Route>
-			<Route path="/register" element={<Register />}></Route>
-			<Route path="/" element={<DefaultLayout />}>
-				<Route path="/" element={<MyPortfolio />}></Route>
-				<Route path="/discover" element={<Discover />}></Route>
-			</Route>
-		</Routes>
-	)
+  function isUserLoggedIn() {
+    const isUserLoggedIn = Boolean(
+      document.cookie
+        .split('; ')
+        .find((cookie) => cookie.startsWith('is-logged-in='))
+        ?.split('=')[1],
+    )
+
+    return isUserLoggedIn
+  }
+
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={isUserLoggedIn() ? <Navigate to="/" /> : <Login />}
+      ></Route>
+      <Route
+        path="/register"
+        element={isUserLoggedIn() ? <Navigate to="/" /> : <Register />}
+      ></Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route
+          path="/"
+          element={
+            isUserLoggedIn() ? <MyPortfolio /> : <Navigate to="/login" />
+          }
+        ></Route>
+        <Route
+          path="/discover"
+          element={isUserLoggedIn() ? <Discover /> : <Navigate to="/login" />}
+        ></Route>
+      </Route>
+    </Routes>
+  )
 }
