3fe71b89ab04789a87e8cd5c2f188e5be4aca70b
Giovani de Oliveira <giovani98@proton.me>
2024-02-05 17:11:14 -0300

    feat: Configura variáveis de ambiente do firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,25 +1,27 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+// import * as dotenv from 'dotenv'
+// dotenv.config()
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCB0NDUZq5j8PKQpHsygGKXscwU1Ab-Gkk",
-  authDomain: "orange-portfolio-67245.firebaseapp.com",
-  projectId: "orange-portfolio-67245",
-  storageBucket: "orange-portfolio-67245.appspot.com",
-  messagingSenderId: "143197120968",
-  appId: "1:143197120968:web:1e5c945d5372917108b8fd",
-  measurementId: "G-9GD1R1YST0",
-};
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
-const auth = getAuth(app);
-auth.useDeviceLanguage();
+const auth = getAuth(app)
+auth.useDeviceLanguage()
 
-export { auth };
+export { auth }
