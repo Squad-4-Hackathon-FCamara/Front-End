df687032a19985b5c9f696e0bbcc66f86636bbfe
Giovani de Oliveira <giovani98@proton.me>
2024-02-05 18:38:39 -0300

    fix: Altera URL para deploy

--- src/AxiosConfig.ts
+++ src/AxiosConfig.ts
@@ -1,20 +1,20 @@
-import axios from "axios";
+import axios from 'axios'
 
 // Substituir a baseUrl pela do nosso back end
 export const AxiosAPI = axios.create({
-  baseURL: "http://localhost:3001",
-  // baseURL: 'https://orange-portfolio-3fgq.onrender.com/',
+  // baseURL: "http://localhost:3001",
+  baseURL: 'https://orange-portfolio-3fgq.onrender.com/',
   headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": [
-      "OPTIONS",
-      "POST",
-      "PUT",
-      "GET",
-      "DELETE",
-      "PATCH",
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': [
+      'OPTIONS',
+      'POST',
+      'PUT',
+      'GET',
+      'DELETE',
+      'PATCH',
     ],
   },
   withCredentials: true,
-});
+})
